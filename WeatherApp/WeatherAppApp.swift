1989a1cae4a3632e026dfba7ff21631ae19fec93
Zhancheng Song <zhanchengsong@Zhanchengs-MacBook-Pro.local>
2021-12-19 14:06:35 -0500

    Initial Creation of app

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Zhancheng Song on 2021-12-19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
